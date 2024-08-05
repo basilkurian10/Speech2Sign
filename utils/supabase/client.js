d47d7e1a0ce572a6f16bfa8eb88d2326db61433f
basilkurian10 <basilkurian.ka@gmail.com>
2024-08-05 14:58:51 -0400

    3 commit

--- utils/supabase/client.js
+++
@@ -1,9 +0,0 @@
-// server.js
-import { createClient } from '@supabase/supabase-js';
-
-export function getSupabaseClient() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
-}
